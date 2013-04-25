9e7f0a250cedac9159a1c627e9cdb66ef50f5055
bencode <bencode@163.com>
2013-04-25 20:30:09 +0800

    bugfix

--- js/loader/alias.js
+++ js/loader/alias.js
@@ -3,17 +3,19 @@
  */
 define('alias', ['util'], function(util) {
 	
-var map = {}
-
-return {
+var Alias = {
 	get: function(config, id) {
-		var alias = map[id] || (config.alias && config.alias[id]);
+		var alias = this._cache[id] || (config.alias && config.alias[id]);
 		return alias || id;
 	},
 
 	push: function(o) {
-		util.extend(map, o);	
-	}
+		util.extend(this._cache, o);	
+	},
+
+	_cache: {}
 };
+
+return Alias;
 
 });
